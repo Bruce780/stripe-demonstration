25797f14c0521002a00b338cc511a7d028f01e6c
Bruce780 <ieghosa4@gmail.com>
2025-07-21 00:55:02 +0100

    first commit

---
+++ server.js
@@ -0,0 +1,32 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const Stripe = require('stripe');
+const stripe = Stripe(process.env.STRIPE_SECRET_KEY);
+
+const PORT = process.env.PORT || 4242;
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.post('/create-payment-intent', async (req, res) => {
+  const { amount } = req.body; // this is to showe amount in cents (e.g., $10 = 1000)
+
+  try {
+    const paymentIntent = await stripe.paymentIntents.create({
+      amount,
+      currency: 'usd',
+      payment_method_types: ['card'],
+    });
+
+    res.send({ clientSecret: paymentIntent.client_secret });
+  } catch (err) {
+    res.status(500).send({ error: err.message });
+  }
+});
+
+app.listen(4242, () => console.log('Server running on http://localhost:4242'));
