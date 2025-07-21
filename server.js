836008e906ee44739ad6296504081d6e19980623
Bruce780 <66451628+Bruce780@users.noreply.github.com>
2025-07-21 01:20:02 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 const express = require('express');
+const app = express();
 const cors = require('cors');
 const Stripe = require('stripe');
 const stripe = Stripe(process.env.STRIPE_SECRET_KEY);
@@ -9,7 +10,7 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
 
-const app = express();
+
 app.use(cors());
 app.use(express.json());
 
